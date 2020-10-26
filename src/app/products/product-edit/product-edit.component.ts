5f6e712b95d0b66bb2d5176c9b4af3a15af0c93e
spvashista79 <spvashista79@gmail.com>
2020-10-26 13:07:11 +0530

    More changes

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -10,9 +10,24 @@ import { ProductService } from '../product.service';
 })
 export class ProductEditComponent implements OnInit {
   pageTitle: string = 'Product Edit';
-  product: Product;
+  //product: Product;
   errorMessage: string;
   private dataIsValid: { [key: string]: boolean } = {};
+  get isDirty(): boolean {
+    return JSON.stringify(this.originalProduct) !== JSON.stringify(this.currentProduct);
+  }
+  
+  private currentProduct: Product;
+  private originalProduct: Product;
+
+  get product(): Product {
+    return this.currentProduct;
+  }
+  set product(value: Product) {
+    this.currentProduct = value;
+    // Clone the object to retain a copy
+    this.originalProduct = value ? { ...value } : null;
+  }
   constructor(private route: ActivatedRoute,private productService:ProductService,
               private router: Router,private messageService: MessageService) { }
 
@@ -42,9 +57,40 @@ export class ProductEditComponent implements OnInit {
       }
     }
   }
- 
+
+  
+  validate(): void {
+    // Clear the validation object
+    this.dataIsValid = {};
+
+
+    if (this.product.productName &&
+      this.product.productName.length >= 3 &&
+      this.product.productCode) {
+      this.dataIsValid['productName'] = true;
+    } else {
+      this.dataIsValid['productName'] = false;
+    }
+
+    
+    if (this.product.productCode &&
+      this.product.productCode.length >= 3) {
+      this.dataIsValid['productCode'] = true;
+    } else {
+      this.dataIsValid['productCode'] = false;
+    }
+  }
+
+  isValid(path?: string): boolean {
+    this.validate();
+    if (path) {
+      return this.dataIsValid[path];
+    }
+    return (this.dataIsValid &&
+      Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true));
+  }
   onProductSave(): void {
-    if (true===true) {
+    if (this.isValid()) {
       if (this.product.id === 0) {
         this.productService.createProduct(this.product).subscribe({
           next: () => this.onSaveComplete(`The new ${this.product.productName} was saved`),
